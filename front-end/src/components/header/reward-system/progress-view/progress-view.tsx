c0f553d06ff2e7bd22a422e8287a9b10ae5128b8
Bianca Mihaela Tizu <b.tizu@levi9.com>
2023-03-13 21:11:35 +0200

    Added life and experience progress bar

--- front-end/src/components/header/reward-system/progress-view/progress-view.tsx
+++ front-end/src/components/header/reward-system/progress-view/progress-view.tsx
@@ -1,12 +1,28 @@
-import { Divider } from "antd";
+import { useState } from "react";
+
+import { HeartTwoTone, StarTwoTone } from "@ant-design/icons";
+import { Progress } from "antd";
 import Card from "antd/lib/card/Card";
 
+import "./progress-view.css";
+
 const ProgressView = () => {
+  const [lifePercent, setLifePercent] = useState(0);
+  const [experiencePercent, setExperiencePercent] = useState(0);
+
   return (
     <div className="progress-wrapper">
-      <h3 className="progress-title">The Challenging Side</h3>
-      <Card title="Progress">
-        <p>Content</p>
+      <h3 className="title">The Challenging Side</h3>
+      <Card title="Progress" bordered={false}>
+        <div className="progress-item">
+          <HeartTwoTone twoToneColor="#eb2f96" />
+          <Progress percent={lifePercent} status="active" />
+        </div>
+
+        <div className="progress-item">
+          <StarTwoTone />
+          <Progress percent={experiencePercent} status="active" />
+        </div>
       </Card>
       <Card title="Challenges">
         <p>Content</p>
